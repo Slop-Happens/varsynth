0c3beb52868c7ec77fe27df687970fa9231b3e1d
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-18 11:02:49 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            gofumpt
+            golangci-lint
+            delve
+            air
+          ];
+
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            export GOPATH="$PWD/.go"
+            export GOBIN="$GOPATH/bin"
+            export PATH="$GOBIN:$PATH"
+
+            mkdir -p "$GOPATH" "$GOBIN"
+
+            echo "Go dev shell ready: $(go version)"
+          '';
+        };
+      }
+    );
+}
